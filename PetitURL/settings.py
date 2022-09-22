d11e1b219bbbd914b8b1aabcf9d86c3cb47d6db1
Soham Biswas <sohambiswas41@gmail.com>
2022-09-22 19:53:28 +0530

    add csrf trusted origin

--- PetitURL/settings.py
+++ PetitURL/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app']
 
+CSRF_TRUSTED_ORIGINS = [['https://*.railway.app', 'https://*.127.0.0.1']]
 
 # Application definition
 
